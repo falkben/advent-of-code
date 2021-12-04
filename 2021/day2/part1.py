54d7907b6807606fff8b8566ffbfdaf892fa4bb7
Ben Falk <falk.ben@gmail.com>
2021-12-04 01:00:37 -0500

    day2 and day3

---
+++ 2021/day2/part1.py
@@ -0,0 +1,28 @@
+input = """\
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"""
+
+# part 1
+
+with open("2021/day2/data.txt") as inputfh:
+    data = inputfh.read()
+input = data
+
+depth = 0
+hor = 0
+
+for l in input.splitlines():
+    dir, amount = l.split(" ")
+    amount = int(amount)
+    if dir == "forward":
+        hor += amount
+    elif dir == "down":
+        depth += amount
+    else:
+        depth -= amount
+
+print(depth * hor)
---
+++ 2021/day3/part1.py
@@ -0,0 +1,39 @@
+from io import StringIO
+
+import numpy as np
+
+input = """\
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"""
+
+with open("2021/day3/data.txt") as fh:
+    input = fh.read()
+
+
+c = StringIO(input)
+arr = np.genfromtxt(c, dtype=int, delimiter=np.ones(12, int))
+
+arr_avg = np.mean(arr, axis=0)
+
+gamma_bin = np.round(arr_avg).astype(int)
+epsilon_bin = np.abs(1 - gamma_bin)
+
+
+def conv_arr_dec(arr):
+    dec = 0
+    for i, v in enumerate(reversed(arr)):
+        dec += v * 2 ** i
+    return dec
+
+
+print("power consumption:", conv_arr_dec(gamma_bin) * conv_arr_dec(epsilon_bin))
