622edfa83628a7acfa3b22681852354176b3602b
Ben Falk <bfalk@stsci.edu>
2021-12-07 17:29:13 -0500

    part2 -- calc cost across all pos values

--- 2021/day7/part2.py
+++ 2021/day7/part2.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-input = "16,1,2,0,4,2,7,1,2,14"
+# input = "16,1,2,0,4,2,7,1,2,14"
 
-# with open("2021/day7/data.txt") as fh:
-#     input = fh.read()
+with open("2021/day7/data.txt") as fh:
+    input = fh.read()
 
-pos = np.fromstring(input, dtype=np.int16, sep=",")
+pos = np.fromstring(input, dtype=int, sep=",")
 
 # cost for each move
 # cost for a 3 move = 1 + 2 + 3 = n * (n + 1) / 2
@@ -15,12 +15,12 @@ pos = np.fromstring(input, dtype=np.int16, sep=",")
 max_pos = pos.max()
 min_pos = pos.min()
 
-check_vals = np.zeros((pos.shape[0], max_pos), dtype=np.int16)
+check_vals = np.dot(np.ones(pos.shape, dtype=int)[:, None], np.arange(min_pos, max_pos + 1, dtype=int)[None, :])
+pos_vals = np.dot(pos[:, None], np.ones(check_vals.shape[1], dtype=int)[None, :])
 
-for i in range(max_pos):
-    check_vals[:, i] = i
+cost = np.abs(pos_vals - check_vals) * (np.abs(pos_vals - check_vals) + 1) / 2
+target = np.argmin(np.sum(cost, axis=0))
 
-target = np.round(np.mean(pos))
 fuel = np.sum(np.abs(pos - target) * (np.abs(pos - target) + 1) / 2)
 
 print(fuel)
