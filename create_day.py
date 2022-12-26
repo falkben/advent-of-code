c31310c0de336fa0f0adf728556eaf5538a58f86
Ben Falk <falk.ben@gmail.com>
2022-12-26 11:36:10 -0500

    create_day script downloads data too

--- create_day.py
+++ create_day.py
@@ -3,9 +3,27 @@
 from __future__ import annotations
 
 import argparse
+import os
 from datetime import date
 from pathlib import Path
 from typing import Sequence
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def download_input(year: str, day: int):
+    session_token = os.getenv("session")
+    if session_token is None:
+        raise TypeError("Set .env file with session token")
+    resp = requests.get(
+        f"https://adventofcode.com/{year}/day/{day}/input",
+        cookies={"session": session_token},
+    )
+    resp.raise_for_status()
+    return resp.text
 
 
 def main(argv: Sequence[str] | None = None):
@@ -25,11 +43,16 @@ def main(argv: Sequence[str] | None = None):
     p.mkdir(parents=True, exist_ok=True)
     for part in ["part1.py", "part2.py"]:
         if not (p / part).exists():
-            (p / part).open(mode="w")
+            with open((p / part), mode="w") as dayfile:
+                dayfile.write(f'# input = open("{p}/data.txt")')
             print(f"Created path {p / part}")
         else:
             print(f"Path {p / part} already found, exiting")
             return 1
+    input = download_input(year, args.day)
+    with open(p / "data.txt", mode="w") as input_file:
+        input_file.write(input)
+
     return 0
 
 
